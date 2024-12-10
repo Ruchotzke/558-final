4124dafefeca72bbd5fff5436ea32bd8c255480b
Ethan Ruchotzke <35568374+Ruchotzke@users.noreply.github.com>
2024-12-09 20:49:32 -0600

    Network set up

--- src/main.py
+++ src/main.py
@@ -19,9 +19,8 @@ logger.instance.LOG_LEVEL = Level.DEBUG
 #
 # Network Vision
 #
-#  1         2         3
-# N1        N2        N3
-#  |        ||        |
+# N1 N2 N3  R1        S1
+#  |  |  |  ||        |
 #   ========  ========
 #    x.x.0.0   x.x.1.0
 
@@ -29,30 +28,34 @@ logger.instance.LOG_LEVEL = Level.DEBUG
 # Generate left network
 N1 = Node(env, "N1")
 N2 = Node(env, "N2")
+N3 = Node(env, "N3")
 left_net = Network(env, IPAddr("192.168.0.0"))
 
 N1.add_interface(left_net, EthernetAddr("00:11:11:11:11:11"), IPAddr("192.168.0.1"))
 N2.add_interface(left_net, EthernetAddr("00:22:22:22:22:22"), IPAddr("192.168.0.2"))
+N3.add_interface(left_net, EthernetAddr("00:33:33:33:33:33"), IPAddr("192.168.0.3"))
 
 # Generate right network
-N3 = Node(env, "N3")
+S1 = Node(env, "S1")
 right_net = Network(env, IPAddr("192.168.1.0"))
 
-N2.add_interface(right_net, EthernetAddr("11:22:22:22:22:22"), IPAddr("192.168.1.2"))
-N3.add_interface(right_net, EthernetAddr("11:33:33:33:33:33"), IPAddr("192.168.1.3"))
+S1.add_interface(right_net, EthernetAddr("11:FF:FF:FF:FF:FF"), IPAddr("192.168.1.1"))
 
-# Mark N2 as a router
-N2.stack.set_router(True)
+# Generate router
+R1 = Node(env, "R1")
+R1.add_interface(left_net, EthernetAddr("00:44:44:44:44:44"), IPAddr("192.168.0.254"))
+R1.add_interface(right_net, EthernetAddr("11:44:44:44:44:44"), IPAddr("192.168.1.254"))
+R1.stack.set_router(True)
 
 # Network setup
 nets = [left_net, right_net]
 RouteGenerator.update_routes(nets)
 
 # Install an app
-app = GeneratorApp(env, N1, IPAddr("192.168.1.3"), 22)
+app = GeneratorApp(env, N1, IPAddr("192.168.1.1"), 22)
 N1.install_app(app)
-app2 = ListenerApp(env, N3, 22)
-N3.install_app(app2)
+app2 = ListenerApp(env, S1, 22)
+S1.install_app(app2)
 
 # Leave gap in the log
 Logger.instance.log_chapter("STARTING SIMULATION")
